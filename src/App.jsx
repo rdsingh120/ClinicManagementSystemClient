99ff98df8682a2700fcdc5ba856882367ff66157
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-07 20:35:00 +0530

    add public and protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
 import { Route, Routes } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import { ToastContainer } from 'react-toastify'
 import Dashboard from './pages/Dashboard'
-
+import SignInPage from './pages/SignInPage'
+import ProtectedRoute from './components/ProtectedRoute'
+import PublicRoute from './components/PublicRoute'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/signin" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/signin"
+          element={
+            <PublicRoute>
+              <SignInPage />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute>
+              <SignUpPage />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/dashboard/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="profile" element={<ProfilePage />} />
+        </Route>
       </Routes>
       <ToastContainer
         position="top-right"
