cf7271b33a26fda5b20883c958966701cbb07d6d
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-08 00:20:41 +0530

    Update ProfilePage.jsx

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,18 +1,68 @@
-import { useState } from "react"
-import { useOutletContext } from "react-router-dom"
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useOutletContext } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 const ProfilePage = () => {
-    const [input, setInput] = useState({})
-    const {user} = useOutletContext()
+  const [input, setInput] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    address: '',
+  })
+  const { user } = useOutletContext()
+  useEffect(() => {
+    if (user) {
+      const profile = user.patientProfile || {}
+      setInput({
+        firstName: user.firstName || '',
+        lastName: user.lastName || '',
+        email: user.email || '',
+        phone: profile.phone || '',
+        address: profile.address || '',
+      })
+    }
+  }, [user])
 
-    const handleInputState = (e) =>
-      setInput({ ...user, [e.target.name]: e.target.value })
-    
+  const handleInputState = (e) => {
+    const { name, value } = e.target
+    setInput((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      }
+    })
+  }
+
+  const updateProfile = async (e) => {
+    e.preventDefault()
+    const payload = {
+      firstName: input.firstName,
+      lastName: input.lastName,
+      email: input.email,
+      patientProfile: {
+        phone: input.phone,
+        address: input.address,
+      },
+    }
+    try {
+      const { data } = await axios.put(
+        `http://localhost:3000/api/user/${user._id}`,
+        payload
+      )
+      return toast.success(data.message)
+    } catch (error) {
+      return toast.error(error.message)
+    }
+  }
+
   return (
     <div className="bg-white flex-1 p-6 overflow-y-auto rounded-tl-2xl">
       <form
         action=""
         className="max-w-[600px] mx-auto flex flex-col items-center gap-5 p-5 rounded"
+        onSubmit={updateProfile}
       >
         <h1 className="text-2xl text-center text-black font-bold">
           Update Your Profile
@@ -22,7 +72,7 @@ const ProfilePage = () => {
             type="text"
             placeholder="First Name"
             name="firstName"
-            value={user.firstName}
+            value={input.firstName}
             onChange={handleInputState}
             className="border border-black px-4 py-2 w-full"
           />
@@ -30,75 +80,28 @@ const ProfilePage = () => {
             type="text"
             placeholder="Last Name"
             name="lastName"
-            value={user.lastName}
+            value={input.lastName}
             onChange={handleInputState}
             className="border border-black px-4 py-2 w-full"
           />
         </div>
 
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          value={user.email}
-          onChange={handleInputState}
-          className="border border-black px-4 py-2 w-full"
-        />
-
-        <input
-          type="text"
-          placeholder="Health Card Number"
-          name="healthCardNumber"
-          value=""
-          className="border border-black px-4 py-2 w-full"
-        />
-
-        <div className="w-full flex items-end gap-2">
-          <div className="w-full">
-            <label htmlFor="" className="text-lg text-black font-bold">
-              Date of Birth
-            </label>
-            <input
-              type="date"
-              name="dob"
-              value=""
-              className="border border-black px-4 py-2 w-full h-[43px]"
-            />
-          </div>
-
-          <div className="w-full">
-            <label htmlFor="" className="text-lg text-black font-bold">
-              Sex
-            </label>
-            <select name="sex" className="border border-black px-4 py-2 w-full">
-              <option value="MALE">Male</option>
-              <option value="FEMALE">Female</option>
-            </select>
-          </div>
-        </div>
-
-        <div className="w-full flex gap-2">
-          <select
-            name="bloodGroup"
+        <div className="flex gap-2 w-full">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={input.email}
+            onChange={handleInputState}
             className="border border-black px-4 py-2 w-full"
-          >
-            <option value="" disabled>
-              Blood Group
-            </option>
-            <option value="A+">A+</option>
-            <option value="A-">A-</option>
-            <option value="B+">B+</option>
-            <option value="AB+">AB+</option>
-            <option value="AB-">AB-</option>
-            <option value="O+">O+</option>
-            <option value="O-">O-</option>
-          </select>
+          />
 
           <input
             type="text"
             placeholder="Phone Number"
             name="phone"
-            value=""
+            value={input.phone}
+            onChange={handleInputState}
             className="border border-black px-4 py-2 w-full"
           />
         </div>
@@ -107,20 +110,14 @@ const ProfilePage = () => {
           type="text"
           placeholder="Address"
           name="address"
-          value=""
+          value={input.address}
+          onChange={handleInputState}
           className="border border-black px-4 py-2 w-full"
         />
 
-        <div className="flex justify-start items-center gap-5 w-full">
-          <label htmlFor="">
-            In the event of my death, I concent to donate my organs
-          </label>
-          <input type="checkbox" name="isOrganDonor" id="" />
-        </div>
-
         <input
           type="submit"
-          value={'Update Profile'}
+          value="Update Profile"
           className="bg-blue-500 px-4 py-2 text-white font-medium cursor-pointer w-full"
         />
       </form>
