d9da654e5d796277f3c09aa6e967d558a236b67f
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-07 15:39:36 +0530

    fix: sidebar white space issue

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { CgProfile } from 'react-icons/cg'
 
 const Sidebar = () => {
   return (
-    <div className="w-full md:w-[300px] h-[calc(100vh-100px)] fixed top-24 left-0 bg-blue-500 px-5">
+    <div className="w-full md:w-[300px] h-[calc(100vh-100px)] fixed top-[5.9rem] left-0 bg-blue-500 px-5">
       <div className="mt-10 flex flex-col gap-5">
         <div className="bg-white rounded-lg px-4 py-1 cursor-pointer flex items-center gap-5">
           <CgProfile className="text-xl" />
