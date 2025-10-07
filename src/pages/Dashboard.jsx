91204acd216698a4d5ce0fbb9010b8e6fb8ded29
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-07 20:35:36 +0530

    add fetch methods

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,48 @@
+import { useState } from 'react'
 import Footer from '../components/footer'
 import MainContent from '../components/MainContent'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/sidebar'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { Outlet } from 'react-router-dom'
 const Dashboard = () => {
+  const [userId, setUserId] = useState('')
+  const [user, setUser] = useState({})
+  const token = localStorage.getItem('token')
+
+  const fetchUserId = async () => {
+    const { data } = await axios.get('http://localhost:3000/api/dashboard', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    setUserId(data?.user?.id)
+  }
+
+  const fetchUser = async (id) => {
+    const { data } = await axios.get(`http://localhost:3000/api/user/${id}`)
+    setUser(data?.user)
+  }
+  useEffect(() => {
+    fetchUserId()
+  }, [])
+
+  useEffect(() => {
+    fetchUser(userId)
+  }, [userId])
+
+  console.log(user);
+  
+const fullName = `${user?.firstName} ${user?.lastName}`
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
+      <Navbar name={fullName} role={user?.role} />
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
         <div className="flex flex-col flex-1 md:ml-[300px] overflow-y-auto">
-          <MainContent />
+          <Outlet context={{user}}/>
           <Footer />
         </div>
       </div>
