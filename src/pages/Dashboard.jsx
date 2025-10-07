8d72fd5306dd7834fc3b851c0f94a66f2863101f
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-07 15:26:05 +0530

    fix: dashboard layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,19 @@
+import Footer from '../components/footer'
+import MainContent from '../components/MainContent'
 import Navbar from '../components/Navbar'
+import Sidebar from '../components/sidebar'
 const Dashboard = () => {
   return (
-    <>
-    <Navbar/>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar />
+        <div className="flex flex-col flex-1 md:ml-[300px] overflow-y-auto">
+          <MainContent />
+          <Footer />
+        </div>
+      </div>
+    </div>
   )
 }
 export default Dashboard
