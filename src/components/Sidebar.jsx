f0158fb5d679fdb4982a68db88d9ff7ea050171c
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-09 00:02:23 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { CgProfile } from 'react-icons/cg'
-import { FaBriefcaseMedical } from 'react-icons/fa6'
+import { MdEditDocument } from 'react-icons/md'
 import { useNavigate } from 'react-router-dom'
 
 const Sidebar = () => {
@@ -8,19 +8,19 @@ const Sidebar = () => {
     <div className="w-full md:w-[300px] h-[calc(100vh-100px)] fixed top-[5.9rem] left-0 bg-blue-500 px-5">
       <div className="mt-10 flex flex-col gap-5">
         <div
-          onClick={() => navigate('/dashboard/profile')}
+          onClick={() => navigate('/dashboard')}
           className="bg-white rounded-lg px-4 py-1 cursor-pointer flex items-center gap-5"
         >
           <CgProfile className="text-xl" />
-          <span className="text-lg">Profile</span>
+          <span className="text-lg">Patient Profile</span>
         </div>
 
         <div
-          onClick={() => navigate('/dashboard/medical-details')}
+          onClick={() => navigate('/dashboard/profile')}
           className="bg-white rounded-lg px-4 py-1 cursor-pointer flex items-center gap-5"
         >
-          <FaBriefcaseMedical className="text-xl" />
-          <span className="text-lg">Medical Details</span>
+          <MdEditDocument className="text-xl" />
+          <span className="text-lg">Update Profile</span>
         </div>
       </div>
     </div>
