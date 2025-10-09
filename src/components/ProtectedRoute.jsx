6b5c94c9699f1e6c9f53b3d1d3fdadc191e2c9a7
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-09 20:24:49 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,10 @@
-import { Navigate } from "react-router-dom"
+import { Navigate } from 'react-router-dom'
 
-const ProtectedRoute = ({children}) => {
-    const token = localStorage.getItem('token')
-
-    if(!token) {
-        return <Navigate to={"/signin"} replace />
-    }
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem('token')
+  if (!token) {
+    return <Navigate to={'/signin'} replace />
+  }
   return children
 }
 export default ProtectedRoute
