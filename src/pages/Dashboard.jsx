28e32604defee6f80d77cd440749984d38183750
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-09 20:26:26 +0530

    remove: fetch user and fetch userid

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,55 +1,20 @@
-import { useState } from 'react'
+import { useContext } from 'react'
 import Footer from '../components/footer'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/sidebar'
-import axios from 'axios'
-import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
+import { UserContext } from '../context/UserContext'
 const Dashboard = () => {
-  const [userId, setUserId] = useState('')
-  const [user, setUser] = useState({})
-  const token = localStorage.getItem('token')
-
-  const fetchUserId = async () => {
-    try {
-      const { data } = await axios.get('http://localhost:3000/api/dashboard', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      setUserId(data?.user?.id)
-
-    } catch (error) {
-      console.log(error.message);
-      
-    }
-  }
-
-  const fetchUser = async (id) => {
-    try {
-      const { data } = await axios.get(`http://localhost:3000/api/user/${id}`)
-      setUser(data?.user)
-    } catch (error) {
-      console.log(error.message)
-    }
-  }
-  useEffect(() => {
-    fetchUserId()
-  }, [])
-
-  useEffect(() => {
-    if(userId) fetchUser(userId)
-  }, [userId])
-
+  const { user } = useContext(UserContext)
   
-const fullName = `${user?.firstName} ${user?.lastName}`
+  const fullName = `${user?.firstName} ${user?.lastName}`
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar name={fullName} role={user?.role} />
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
         <div className="flex flex-col flex-1 md:ml-[300px] overflow-y-auto">
-          <Outlet context={{user}}/>
+          <Outlet/>
           <Footer />
         </div>
       </div>
