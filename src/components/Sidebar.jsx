015b810ee29dcc4afa97f6130420351dd8167f46
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-09 05:33:35 +0530

    Update Sidebar links and add 'Find a doctor' option

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,14 @@ const Sidebar = () => {
     <div className="w-full md:w-[300px] h-[calc(100vh-100px)] fixed top-[5.9rem] left-0 bg-blue-500 px-5">
       <div className="mt-10 flex flex-col gap-5">
         <div
+          onClick={() => navigate('/dashboard/find-a-doctor')}
+          className="bg-white rounded-lg px-4 py-1 cursor-pointer flex items-center gap-5"
+        >
+          <CgProfile className="text-xl" />
+          <span className="text-lg">Find a doctor</span>
+        </div>
+
+        <div
           onClick={() => navigate('/dashboard')}
           className="bg-white rounded-lg px-4 py-1 cursor-pointer flex items-center gap-5"
         >
@@ -16,7 +24,7 @@ const Sidebar = () => {
         </div>
 
         <div
-          onClick={() => navigate('/dashboard/profile')}
+          onClick={() => navigate('/dashboard/update-profile')}
           className="bg-white rounded-lg px-4 py-1 cursor-pointer flex items-center gap-5"
         >
           <MdEditDocument className="text-xl" />
